2714ca0d9519c534a5df120aac1624778b71aad2
Vuk Popovic <vukpop2005@gmail.com>
2026-07-10 18:39:29 +0200

    Sredjen Client i dodata Komunikacija

--- src/Projekat/Client.java
+++ src/Projekat/Client.java
@@ -1,6 +1,8 @@
 package Projekat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -13,17 +15,14 @@ public class Client {
     public static void main(String[] args) {
         System.out.println("Pokretanje klijenta i povezivanje na server...");
 
-        // Dodali smo PrintWriter i Scanner direktno u try da se sami zatvore na kraju
         try (Socket socket = new Socket(SERVER_ADDRESS, PORT);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              Scanner scanner = new Scanner(System.in)) {
 
             System.out.println("Uspješno povezivanje sa serverom na adresi: " + socket.getRemoteSocketAddress());
 
-            // Pozivamo tvoju novu klasu za prijavu
-            String korisnickoIme = PrijavaKorisnika.izvrsiPrijavu(out, scanner);
-
-            System.out.println("Veza sa serverom je aktivna. Zatvaranje klijenta...");
+            Komunikacija.pokreniAplikaciju(in, out, scanner);
 
         } catch (UnknownHostException e) {
             System.err.println("Server nije pronađen na adresi: " + SERVER_ADDRESS + " (" + e.getMessage() + ")");
