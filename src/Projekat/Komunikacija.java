1e985f2eb6384230999dceee21562260d1cbc8cd
Vuk Popovic <vukpop2005@gmail.com>
2026-07-11 21:08:36 +0200

    Dodan Izbor Soba

--- src/Projekat/Komunikacija.java
+++ src/Projekat/Komunikacija.java
@@ -10,6 +10,31 @@ public class Komunikacija {
 
         String korisnickoIme = PrijavaKorisnika.izvrsiPrijavu(out, scanner);
 
+        System.out.println("\n=== GLAVNI MENI APLIKACIJE ===");
+        System.out.println("1. Otvori listu tematskih kanala (Chat)");
+        System.out.println("2. Tehnicka podrska (Ticketing System)");
+        System.out.print("Izaberite opciju (1 ili 2): ");
+
+        String opcija = scanner.nextLine();
+
+        if (opcija.equals("1")) {
+            System.out.println("\n=== DOSTUPNI TEMATSKI KANALI ===");
+            System.out.println("1. Programiranje");
+            System.out.println("2. Dizajn");
+            System.out.println("3. Opste teme");
+            System.out.print("Izaberite broj kanala za ulazak: ");
+
+            String izabraniKanal = scanner.nextLine();
+
+            out.println("KANAL:" + izabraniKanal);
+            System.out.println("\nUspjesno ste usli u kanal!");
+        }
+
+        else if (opcija.equals("2")) {
+            out.println("TIKET_SISTEM");
+            System.out.println("\nUlazak u sistem tehnicke podrske...");
+        }
+
         System.out.println("\n=== CHAT JE AKTIVAN ===");
         System.out.println("Mozete poceti sa kucanjem poruka (Za izlaz ukucajte /izlaz)...\n");
 
